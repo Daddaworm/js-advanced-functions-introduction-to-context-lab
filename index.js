214b4c2989a574b54c0587dae1e0246e44037648
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-05-16 16:58:11 -0400

    commit tree 7dd4a1e3d367205ecfde887abbce7e1deb1d0dc1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+
+
+
+function createEmployeeRecord(employeeInfo) {
+        const employeeRecord = {
+            firstName: employeeInfo[0],
+            familyName: employeeInfo[1],
+            title: employeeInfo[2],
+            payPerHour: employeeInfo[3],
+            timeInEvents: [],
+            timeOutEvents: []
+        };
+    return employeeRecord;
+}
+
+
+function createEmployeeRecords(multipleEmployees) {
+
+}
